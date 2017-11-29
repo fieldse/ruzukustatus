8937b9040aee73ea1b6c584f008602002fcede90
Chris Davies <ferociousturtle@gmail.com>
2017-11-29 12:34:51 -0500

    Update PDF error status

--- src/status-updates.js
+++ src/status-updates.js
@@ -1,14 +1,18 @@
-/* 
-  Status Updates 
+/*
+  Status Updates
   This is a js file so we can comment. Put your status updates below.
-  format like so: 
+  format like so:
   {
     "date": "mm-dd-yyyy",
     "title": "Your message",
     "status": "down|issue|nominal"
   }
 */
-const updates = []
-  
+const updates = [{
+  "date": "11-29-2017",
+  "title": "PDF support is currently down.",
+  "status": "down",
+}];
 
-  export { updates };
+
+export { updates };
